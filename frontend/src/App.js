445dae8022c7ffef5c7abcf3192b82db4f891b3a
Aman Raj <78209808+am4n-raj@users.noreply.github.com>
2022-05-10 22:21:48 +0530

    add: header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
-
+import Navbar from './Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 function App() {
   return (
     <div className="App">
-      <h1>E-Commerce</h1>
+      <BrowserRouter>
+
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Product List page</h1>}></Route>
+        <Route path="/add" element={<h1>Add Product page</h1>}></Route>
+        <Route path="/update" element={<h1>Update Product page</h1>}></Route>
+        <Route path="/profile" element={<h1>Profile page</h1>}></Route>
+        <Route path="/logout" element={<h1>Logout page</h1>}></Route>
+
+      </Routes>      
+      </BrowserRouter>
     </div>
   );
 }
