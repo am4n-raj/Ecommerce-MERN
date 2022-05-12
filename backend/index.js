b4c87536a8276e58e4b749059c3a563e06189d4c
Aman Raj <78209808+am4n-raj@users.noreply.github.com>
2022-05-12 19:26:13 +0530

    mongo db update

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,22 @@
 const express = require('express');
-
+const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
-app.get("/",(req,resp)=>{
-    resp.send("app is live..")
-});
+const connectDB = async ()=>{
+    mongoose.connect('mongodb+srv://admin:aman25@products.jwfjq.mongodb.net/Products?retryWrites=true&w=majority')
+    const productSchema = new mongoose.Schema({});
+    const products = mongoose.model('product', productSchema);
+    // const data = await 
+}
+
+
+// const uri = "mongodb+srv://admin:aman25@products.jwfjq.mongodb.net/Products?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("sample_airbnb").collection("listingsAndReviews");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
 app.listen(5000);
